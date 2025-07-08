d863fef1f3afa4e5cee6b3be276b93305e405f25
Muhammad Naeem <m.naeem66622@outlook.com>
2025-07-09 00:40:02 +0500

    feat: Add justified text styling and adjust layout in Hero section

--- src/pages/sections/Hero/index.tsx
+++ src/pages/sections/Hero/index.tsx
@@ -21,7 +21,7 @@ export const Hero: FC = () => {
 
     return (
         <div className="bg-[#121212] min-h-screen max-h-[768px]">
-            <div className="container mx-auto flex items-center justify-center min-h-screen relative overflow-hidden">
+            <div className="max-w-[1920px] mx-auto flex items-center justify-center min-h-screen relative overflow-hidden">
                 <div className="flex flex-col items-center justify-center text-center lg:max-w-[723px] md:max-w-[90vw] sm:max-w-[95vw] w-full px-2">
                     {/* For "Be a part of" (reveal from top) */}
                     <div style={{ overflow: "hidden" }}>
@@ -30,7 +30,7 @@ export const Hero: FC = () => {
                             animate={isReady ? { y: 0 } : {}}
                             transition={{ duration: 0.8, ease: "easeOut" }}
                         >
-                            <h2 className="text-[#F3F3F3] uppercase lg:text-[40px] md:text-2xl sm:text-xl text-lg leading-tight">
+                            <h2 className="text-[#F3F3F3] uppercase lg:text-[35px] md:text-2xl sm:text-xl text-lg leading-15 tracking-[0.2em]">
                                 Be A Part Of
                             </h2>
                         </motion.div>
@@ -47,7 +47,7 @@ export const Hero: FC = () => {
                                 delay: 0.15,
                             }}
                         >
-                            <h1 className="text-[#D1CABA] uppercase font-bold lg:text-[92px] md:text-5xl sm:text-3xl text-2xl leading-none">
+                            <h1 className="text-[#D1CABA] uppercase font-extralight lg:text-[92px] md:text-5xl sm:text-3xl text-2xl leading-none">
                                 The Movement
                             </h1>
                         </motion.div>
@@ -64,7 +64,7 @@ export const Hero: FC = () => {
                                 delay: 0.3,
                             }}
                         >
-                            <p className="text-[#9E9E9E] text-justify lg:text-lg md:text-base sm:text-sm text-xs mt-2">
+                            <p className="text-[#9E9E9E] text-justify lg:text-lg md:text-base sm:text-sm text-xs mt-2 paragraph">
                                 Project Black Is A Movement Dedicated To
                                 Elevating Black Excellence And Minority
                                 Success—Reshaping Narratives, Fostering
@@ -74,34 +74,40 @@ export const Hero: FC = () => {
                         </motion.div>
                     </div>
                     <div className="flex flex-wrap gap-4 mt-8 justify-center">
-                        <Button variant="chocolateOutline" className="">
+                        <Button
+                            variant="chocolateOutline"
+                            className="text-xl py-5"
+                        >
                             Join the Wishlist
                         </Button>
-                        <Button variant="chocolate" className="text-[#D7D7D7]">
+                        <Button
+                            variant="chocolate"
+                            className="text-[#D7D7D7] text-xl py-[23px]"
+                        >
                             Apply Now
                         </Button>
                     </div>
                 </div>
                 {/* Dots animations */}
                 <Dots
-                    className="w-[60px] absolute -top-4 left-0 rotate-90"
+                    className="w-[80px] absolute -top-4 left-0 rotate-90"
                     animate={isReady}
                 />
                 <Dots
-                    className="w-[60px] absolute top-5 right-0"
+                    className="w-[100px] absolute top-5 right-0"
                     initialRotation={90}
                     animate={isReady}
                 />
                 {/* Clock animation */}
                 <Clock
-                    className="w-[200px] absolute bottom-10 left-15"
+                    className="w-[280px] absolute bottom-10 left-15"
                     animate={isReady}
                 />
                 {/* Images with loading tracking */}
                 <img
                     src={logo}
                     alt=""
-                    className="w-[100px] absolute top-0 left-[50%] -translate-x-1/2"
+                    className="w-[120px] absolute top-5 left-[50%] -translate-x-1/2"
                     onLoad={handleImageLoad}
                 />
                 <motion.img
@@ -110,7 +116,7 @@ export const Hero: FC = () => {
                     animate={isReady ? { opacity: 1, x: 0 } : {}}
                     transition={{ duration: 1, ease: "easeOut" }}
                     alt="Hero 1"
-                    className="absolute -bottom-25 left-0 w-[500px] mix-blend-lighten"
+                    className="absolute -bottom-20 left-0 mix-blend-lighten"
                     onLoad={handleImageLoad}
                 />
                 <motion.img
@@ -119,7 +125,7 @@ export const Hero: FC = () => {
                     animate={isReady ? { opacity: 1, x: 0 } : {}}
                     transition={{ duration: 1, ease: "easeOut" }}
                     alt="Hero 2"
-                    className="absolute bottom-0 right-0 w-[500px] mix-blend-lighten"
+                    className="absolute -bottom-20 right-0 mix-blend-lighten"
                     onLoad={handleImageLoad}
                 />
             </div>
